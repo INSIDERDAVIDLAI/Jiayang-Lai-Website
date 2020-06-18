ae5f6ad05c8e1080f41396bac96323fbff778fd2
INSIDERDAVIDLAI <insiderdavidlai@gmail.com>
2020-06-18 19:51:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,31 @@ var ConsoleManager={
             alert("Console is closed!!!!!")
         }
         ConsoleManager.init();
+
+
+        const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+  
+      let countDown = new Date('Aug 6, 2021 00:08:30').getTime(),
+      
+          x = setInterval(function() {
+      
+            let now = new Date().getTime(),
+                distance = countDown - now;
+      
+            document.getElementById('cd-days').innerText = Math.floor(distance / (day)),
+              document.getElementById('cd-hours').innerText = Math.floor((distance % (day)) / (hour)),
+              document.getElementById('cd-minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+              document.getElementById('cd-seconds').innerText = Math.floor((distance % (minute)) / second);
+            
+            //do something later when target is reached
+            if (distance < 0) {
+                clearInterval(x);
+              jQuery('.countdown').html("<span class=\"cd-message\">It's the 2021 Canada Games!</span>");
+            }
+      
+          }, second)
+      
+      
